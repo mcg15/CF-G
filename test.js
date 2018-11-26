94204148f21ed971a3bdbbe15b516b7b523e44fe
Mel <mcg15@ic.ac.uk>
2018-11-26 20:29:15 +0000

    i dont know

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
-$('div').on('hover', function() {
-  $(this).toggleClass('info');
+$('div').hover(function() {
+  $(this).find('.info').css("display", "block");
 });
+
+// $('div').hover(function() {
+//   $(this).find('.info').css("display", "none");
+// });
+
+// ('.info').hover(
+//   function() {
+//     $(this).show();
+//   }
+//   function() {
+//     $(this).hide();
+//   }
+// )
